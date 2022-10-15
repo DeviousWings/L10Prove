189a5a5ef7c6e5e299f1057b3b70f92d79ddcb38
DeviousWings <nksteele@byui.edu>
2022-10-15 10:52:35 -0500

    Got requirements done

--- receipt.py
+++ receipt.py
@@ -15,41 +15,74 @@ PRODUCT_NAME_INDEX = 1
 PRODUCT_PRICE_INDEX = 2
 
 def main():
-    products_dicrionary = read_dict("products.csv", 0)
+    try:
+        products_dicrionary = read_dict("products.csv", 0)
 
-    print("Requested Items")
-    total_quantity = 0
-    # subtotal = 0.0
-    # sales_tax = 0.0
-    # total = 0.0
-    with open("request.csv", 'rt') as request_list:
-        reader = csv.reader(request_list)
-        next(reader)
-    
-    
-        for row_list in reader:
-            key_value = row_list[0]
-            product_quantity = row_list[1]
-            row_list = products_dicrionary[key_value]
-            description = row_list[1]
-            price = row_list[2]
-            
-            
-            print(f"{description}: {product_quantity} @ ${price}")
-        print()
+        print("Requested Items")
+        discount = 0.1
+        with open("request.csv", 'rt') as request_list:
+            reader = csv.reader(request_list)
+            next(reader)
         
-                
-        for inner_list in reader:
-            quantity = inner_list[QUANTITY_INDEX]
-            total_quantity += quantity
-            print(f"Number of Items: {total_quantity}")
-    print(0)
+            numbers_item = 0
+            subtotal = 0.0
+            sales_tax = .06
+            total = 0.0
 
-    print()
-    print("Thank you for shopping at the Inkom Emporium.")
-    # 8. Get the current date and time from your computer's operating system and print the current date and time.
-    print(f"{current_date_and_time:%a %b %I:%M %p}")
-    
+            for row_list in reader:
+                key_value = row_list[0]
+                product_quantity = int(row_list[1])
+                row_list = products_dicrionary[key_value]
+                description = row_list[1]
+                price = float(row_list[2])
+
+                numbers_item += product_quantity
+                
+                subtotal += (price*product_quantity)
+
+                total_tax = sales_tax * subtotal
+                total = total_tax + subtotal
+                
+                print(f"{description}: {product_quantity} @ ${price}")
+                
+            
+            print()
+            print(f"Number of Items: {numbers_item}")
+            print(f"Subtotal: ${subtotal:.2f}")
+            print(f"Sales Tax: ${total_tax:.2f}")
+            print(f"Total: ${total:.2f}")
+            
+        print()
+        print("Thank you for shopping at the Inkom Emporium.")
+        print(f"{current_date_and_time:%a %b %I:%M %p}")
+        
+        
+ 
+            
+    except FileNotFoundError as not_found_err:
+        print()
+        print(type(not_found_err).__name__, not_found_err, sep=": ")
+        print(f"The item {products_dicrionary} doesn't exist.")
+        print("Run the program again and enter the" \
+        " name of an existing file.")
+        
+    except PermissionError as perm_err:
+        # This code will be executed if the user enters the name
+        # of a file and doesn't have permission to read that file.
+        print()
+        print(type(perm_err).__name__, perm_err, sep=": ")
+        print(f"You don't have permission to read {products_dicrionary}.")
+        print("Run the program again and enter the name" \
+                " of a file that you are allowed to read.")
+        
+    except KeyError as keyerr:
+        
+        print()
+        print(type(keyerr).__name__, keyerr, sep=": ")
+        print(f"Error: unknown product ID in the request.csv file '{key_value}'")
+        
+
+        
 
 def read_dict(filename, key_column_index):
     """Read the contents of a CSV file into a compound
